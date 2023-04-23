d3446994b1e353077e7a579ef8c112573503ae36
yixiangyin <y1027020239@qq.com>
2023-04-23 22:21:18 +1000

    extract video names works okay

--- main.py
+++ main.py
@@ -8,6 +8,12 @@ import time
 import getpass
 from selenium.common.exceptions import NoSuchElementException
 
+# helper function
+def elementHasClass(element, active):
+    print(element.get_attribute("class").split(" "))
+    return active in element.get_attribute("class").split(" ")
+ 
+
 #IF USING A RASPBERRY PI, FIRST INSTALL THIS OPTIMIZED CHROME DRIVER
 #sudo apt-get install chromium-chromedriver
 browser_driver = Service('/usr/lib/chromium-browser/chromedriver')
@@ -21,15 +27,23 @@ page_to_scrape.get("https://search.bilibili.com/all?keyword="+word_to_search)
 
 # go to next page until there is no next page
 while True:
-  try:
-    # page_to_scrape.execute_script("window.scrollTo(0, Y)") 
+    # scroll to the bottom
     page_to_scrape.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-    time.sleep(3)
-    page_to_scrape.find_elements(By.CSS_SELECTOR, "button.vui_pagenation--btn-side")[1].click()
-    time.sleep(3)
-  except NoSuchElementException:
-    print("end of the list!")
-    break;
+    # wait to load
+    time.sleep(2)
+    # process
+    videos = page_to_scrape.find_elements(By.CSS_SELECTOR, ".bili-video-card__info--tit");
+    for video in videos:
+      print(video.get_attribute("title"))
+    next_page_button = page_to_scrape.find_elements(By.CSS_SELECTOR, "button.vui_pagenation--btn-side")[1]
+    if (elementHasClass(next_page_button, "vui_button--disabled")):
+      break;
+    next_page_button.click()
+    # wait to load
+    time.sleep(2)
+
+
+
 # searchBox = page_to_scrape.find_element(By.CLASS_NAME, "nav-search-input")
 # searchBox.send_keys("厨房")
 # page_to_scrape.find_element(By.CLASS_NAME, "nav-search-btn").find_element(By.CSS_SELECTOR, "path").click;
