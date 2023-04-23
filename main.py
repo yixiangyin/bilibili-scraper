915e3716902df2823ed41ac5bdef405ce1190813
yixiangyin <y1027020239@qq.com>
2023-04-23 23:14:11 +1000

    final

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-#INSTALL SELENIUM BEFORE RUNNING THIS CODE
-#pip3 install selenium
-# import csv
+import secrets
+import csv
+import sys
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
@@ -10,68 +10,65 @@ from selenium.common.exceptions import NoSuchElementException
 
 # helper function
 def elementHasClass(element, active):
-    print(element.get_attribute("class").split(" "))
+    # print(element.get_attribute("class").split(" "))
     return active in element.get_attribute("class").split(" ")
  
 
-#IF USING A RASPBERRY PI, FIRST INSTALL THIS OPTIMIZED CHROME DRIVER
-#sudo apt-get install chromium-chromedriver
-browser_driver = Service('/usr/lib/chromium-browser/chromedriver')
-page_to_scrape = webdriver.Chrome(service=browser_driver)
-# page_to_scrape.get("http://quotes.toscrape.com")
-# word_to_search = input("Input the keyword to search")
-word_to_search = "123"
-page_to_scrape.get("https://search.bilibili.com/all?keyword="+word_to_search)
+def main():
+  if len(sys.argv)!=2 and len(sys.argv)!=3:
+    print("usage: python main.py <keyword_to_search> [page_limit]")
+    return
+  if (len(sys.argv)==3):
+    try:
+      limit = int(sys.argv[2])
+      if (limit <= 0):
+        print("page limit must be greater than 0")
+        return 
 
-# get data 
+    except ValueError:
+      print("page limit is not a integer")
+      return
+      
+    page_counter = 0
+  else:
+    limit = page_counter - 1
+  browser_driver = Service('/usr/lib/chromium-browser/chromedriver')
+  page_to_scrape = webdriver.Chrome(service=browser_driver)
+  word_to_search = sys.argv[1]
+  secure_num = secrets.randbelow(500000)
+  # go to the website and use a random number to bypass the checking
+  page_to_scrape.get("https://search.bilibili.com/all?"+"vt="+str(secure_num)+"&"+"keyword="+word_to_search)
+  # go to video tab
+  tabs = page_to_scrape.find_elements(By.CSS_SELECTOR, "span.vui_tabs--nav-text")
+  for tab in tabs:
+    if tab.text == "视频":
+      tab.click()
+      break
+  time.sleep(3)
+  # go to next page until there is no next page
+  while True:
+      # scroll to the bottom
+      page_to_scrape.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+      # wait to load
+      time.sleep(3)
+      # get all the video cards
+      videos = page_to_scrape.find_elements(By.CSS_SELECTOR, ".bili-video-card__info--tit");
+      file = open("scraped_data_"+sys.argv[1]+".csv", "w")
+      # write name to csv
+      writer = csv.writer(file)
+      writer.writerow(["VIDEONAME"])
+      for video in videos:
+        writer.writerow([video.get_attribute("title")])
+      next_page_button = page_to_scrape.find_elements(By.CSS_SELECTOR, "button.vui_pagenation--btn-side")[1]
+      if (elementHasClass(next_page_button, "vui_button--disabled")):
+        break;
+      # go to the next page
+      next_page_button.click()
+      # wait to load
+      time.sleep(3)
+      page_counter+=1
+      if (page_counter == limit):
+        break
+  file.close()
 
-# go to next page until there is no next page
-while True:
-    # scroll to the bottom
-    page_to_scrape.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-    # wait to load
-    time.sleep(2)
-    # process
-    videos = page_to_scrape.find_elements(By.CSS_SELECTOR, ".bili-video-card__info--tit");
-    for video in videos:
-      print(video.get_attribute("title"))
-    next_page_button = page_to_scrape.find_elements(By.CSS_SELECTOR, "button.vui_pagenation--btn-side")[1]
-    if (elementHasClass(next_page_button, "vui_button--disabled")):
-      break;
-    next_page_button.click()
-    # wait to load
-    time.sleep(2)
-
-
-
-# searchBox = page_to_scrape.find_element(By.CLASS_NAME, "nav-search-input")
-# searchBox.send_keys("厨房")
-# page_to_scrape.find_element(By.CLASS_NAME, "nav-search-btn").find_element(By.CSS_SELECTOR, "path").click;
-# username = page_to_scrape.find_element(By.ID, "username")
-# password = page_to_scrape.find_element(By.ID, "password")
-# username.send_keys("admin")
-# #USING GETPASS WILL PROMPT YOU TO ENTER YOUR PASSWORD INSTEAD OF STORING
-# #IT IN CODE. YOU'RE ALSO WELCOME TO USE A PYTHON KEYRING TO STORE PASSWORDS.
-# my_pass = getpass.getpass()
-# password.send_keys(my_pass)
-# page_to_scrape.find_element(By.CSS_SELECTOR, "input.btn-primary").click()
-# 
-# quotes = page_to_scrape.find_elements(By.CLASS_NAME, "text")
-# authors = page_to_scrape.find_elements(By.CLASS_NAME, "author")
-# 
-# # file = open("scraped_quotes.csv", "w")
-# # writer = csv.writer(file)
-# 
-# writer.writerow(["QUOTES", "AUTHORS"])
-# while True:
-#     quotes = page_to_scrape.find_elements(By.CLASS_NAME, "text")
-#     authors = page_to_scrape.find_elements(By.CLASS_NAME, "author")
-#     # for quote, author in zip(quotes, authors):
-#     #     print(quote.text + " - " + author.text)
-#     #     writer.writerow([quote.text, author.text])
-#     try:
-#         page_to_scrape.find_element(By.PARTIAL_LINK_TEXT, "Next").click()
-#     except NoSuchElementException:
-#         break
-# file.close()
-
+main()
